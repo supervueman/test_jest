2e138403418bf2929758315c798c29767492d46f
supervueman <supervueman@gmail.com>
2021-11-03 21:38:10 +0300

    Add async example

---
+++ 03_async/async.spec.js
@@ -0,0 +1,9 @@
+const Ajax = require('./async')
+
+describe('Ajax: echo', () => {
+  test('should return value async', async () => {
+    const result = await Ajax.echo('some data')
+
+    expect(result).toBe('some data')
+  })
+})
