93571be1132d0e19fe797d56f6ec53d8202923a1
supervueman <supervueman@gmail.com>
2021-11-03 20:46:57 +0300

    Add launch json

--- 02_sync/sync.spec.js
+++ 02_sync/sync.spec.js
@@ -1,7 +1,8 @@
 const Lodash = require('./sync');
 
+let _ = new Lodash();
+
 describe('Lodash: compact', () => {
-  let _ = new Lodash();
   let array = [false, 42, 0, '', true, null, 'hello'];
 
   beforeEach(() => {
@@ -30,5 +31,37 @@ describe('Lodash: compact', () => {
     expect(_.compact(array)).not.toContain(0)
     expect(_.compact(array)).not.toContain('')
     expect(_.compact(array)).not.toContain(null)
+  })
+})
+
+describe('Lodash: groupBy', () => {
+  test('should be defined', () => {
+    expect(_.groupBy).toBeDefined()
+    expect(_.groupBy).not.toBeUndefined()
+  })
+
+  test('should group aaray items by Math.floor', () => {
+    const array = [2.2, 2.4, 4.2, 3.1]
+    const result = {
+      2: [2.2, 2.4],
+      4: [4.2],
+      3: [3.1],
+    }
+
+    expect(_.groupBy(array, Math.floor)).toEqual(result)
+  })
+
+  test('should group aaray items by length', () => {
+    const array = ['one', 'two', 'three']
+    const result = {
+      3: ['one', 'two'],
+      5: ['three'],
+    }
+
+    expect(_.groupBy(array, 'length')).toEqual(result)
+  })
+
+  test('should NOT return array', () => {
+    expect(_.groupBy([], Math.trunc)).not.toBeInstanceOf(Array)
   })
 })
