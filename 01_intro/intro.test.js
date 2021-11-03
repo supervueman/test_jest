5962e692744424f0b5ab9224ad599c1511174d12
supervueman <supervueman@gmail.com>
2021-11-03 19:33:39 +0300

    Test

--- 01_intro/intro.test.js
+++ 01_intro/intro.test.js
@@ -6,5 +6,12 @@ test('Sum should return sum of two values', () => {
 })
 
 test('Sum should return value correctly comparing to other', () => {
-  
+  expect(sum(2, 3)).toBeGreaterThan(4);
+  expect(sum(2, 3)).toBeGreaterThanOrEqual(5);
+  expect(sum(2, 3)).toBeLessThan(10);
+  expect(sum(2, 3)).toBeLessThanOrEqual(5);
+})
+
+test('sum should sum 2 float values correctly', () => {
+  expect(sum(0.1, 0.2)).toBeCloseTo(0.3)
 })
